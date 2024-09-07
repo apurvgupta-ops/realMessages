2bed3589be6026970d5fc367edd15216cfb6b5af
Apurv gupta <56751664+apurvgupta-ops@users.noreply.github.com>
2024-09-07 09:37:18 +0000

    ADD : next auth

---
+++ src/app/api/sign-up/auth/[...nextauth]/options.ts
@@ -0,0 +1,63 @@
+import UserModel from "@/models/User";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import dbConnect from "@/lib/dbConnect";
+import { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+
+      async authorize(credentials: any): Promise<any> {
+        await dbConnect();
+
+        try {
+          const user = UserModel.findOne({
+            $or: [
+              { email: credentials.identifier },
+              { userName: credentials.identifier },
+            ],
+          });
+
+          if (!user) {
+            throw new Error("No user found with this email");
+          }
+
+          if (!user.isVerified) {
+            throw new Error(
+              "you are not verified with  this email, please verifed before login"
+            );
+          }
+
+          const isPasswordMatch = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (isPasswordMatch) {
+            return user;
+          } else {
+            throw new Error("Incorrect password");
+          }
+        } catch (error: any) {
+          throw new Error(error);
+        }
+      },
+    }),
+  ],
+
+  pages: {
+    signIn: "/auth/signin",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
